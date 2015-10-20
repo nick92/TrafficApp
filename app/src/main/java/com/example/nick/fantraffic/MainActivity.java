c8446da6a4515760ed560916f345dfe6afbee804
nick wilkins <nickawilkins@hotmail.com>
2015-10-20 13:51:00 +0100

    added JSON and HTTP request and parsing

--- app/src/main/java/com/example/nick/fantraffic/MainActivity.java
+++ app/src/main/java/com/example/nick/fantraffic/MainActivity.java
@@ -8,12 +8,26 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.CardView;
+import android.util.JsonReader;
+import android.util.JsonToken;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private List<Traffic> traffic;
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        initializeData();
 
         mRecyclerView = (RecyclerView)findViewById(R.id.my_recycler_view);
         mRecyclerView.setHasFixedSize(true);
@@ -39,11 +55,13 @@ public class MainActivity extends AppCompatActivity {
         RVAdapter adapter = new RVAdapter(traffic);
         mRecyclerView.setAdapter(adapter);
 
+        view = this.view;
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "refreshing data", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -69,5 +87,103 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initializeData(){
+        String strJsonData = "http://traffic.cit.api.here.com/traffic/6.0/incidents.json" +
+                "?app_id=wa1d1f5iSh0BQqHHtLI5" +
+                "&app_code=br5kbaTStGduNL9LYzPD8w" +
+                "&quadkey=12020330";
+
+        newRequestQueue(strJsonData);
+
+        try {
+            InputStream in = new InputStream() {
+                @Override
+                public int read() throws IOException {
+                    return 0;
+                }
+            };
+
+            in.read(strJsonData.getBytes());
+
+            traffic = readJsonStream(in);
+        }catch(IOException ioex)
+        {}
+        /*
+        traffic.add(new Traffic("Very Good", "10"));
+        traffic.add(new Traffic("Terrible", "60"));
+        traffic.add(new Traffic("Bad", "50"));
+        traffic.add(new Traffic("Good", "20"));*/
+    }
+
+    public List readJsonStream(InputStream in) throws IOException {
+        JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
+        try {
+            return readMessagesArray(reader);
+        }catch(Exception ex){}
+
+        finally{
+            reader.close();
+            return Collections.emptyList();
+        }
+    }
+
+    public List readMessagesArray(JsonReader reader) throws IOException {
+        List messages = new ArrayList();
+
+        reader.beginArray();
+        while (reader.hasNext()) {
+            messages.add(readTraffic(reader));
+        }
+        reader.endArray();
+        return messages;
+    }
+
+    public Traffic readTraffic(JsonReader reader) throws IOException {
+        long id = -1;
+        Traffic traffic = null;
+        String flow = null;
+        String duration = null;
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals("id")) {
+                id = reader.nextLong();
+            } else if (name.equals("text")) {
+                flow = reader.nextString();
+            } else if (name.equals("text")) {
+                duration = reader.nextString();
+            } else {
+                reader.skipValue();
+            }
+        }
+        reader.endObject();
+        return new Traffic(flow, duration);
+    }
+
+    public void newRequestQueue(String url){
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Snackbar.make(view, "Response is: "+ response.substring(0,500), Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Snackbar.make(view, "That didn't work!", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
--- app/src/main/java/com/example/nick/fantraffic/Traffic.java
+++ app/src/main/java/com/example/nick/fantraffic/Traffic.java
@@ -14,14 +14,4 @@ class Traffic {
         this.flow = flow;
         this.duration = duration;
     }
-
-    private List<Traffic> trafficList;
-
-    private void initializeData(){
-        trafficList = new ArrayList<>();
-        trafficList.add(new Traffic("Very Good", "10"));
-        trafficList.add(new Traffic("Terrible", "60"));
-        trafficList.add(new Traffic("Bad", "50"));
-        trafficList.add(new Traffic("Good", "20"));
-    }
 }
